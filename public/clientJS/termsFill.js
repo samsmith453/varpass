9e4cd1bb3bc740d7eef255a759384c434dc6e3bd
samsmith453 <samsmith453@googlemail.com>
2017-08-21 07:36:25 +0100

    analytics

--- public/clientJS/termsFill.js
+++ public/clientJS/termsFill.js
@@ -1,30 +1,33 @@
 var termsFill = {
 	fasttrack: {
-		h3: "FastTrack Terms & Conditions",
-		body: "<p>This is for University of Manchester students only. VarPass will do everything it can to help \
+		h3: "Terms & Conditions",
+		body: "<p>This is for Manchester students only. VarPass will do everything it can to help \
 		you in any way we can. We can't take responsibility for your University application, your accommodation \
 		application, your visa application, or any other administration prior to coming to study in the UK.</p>\
 		<p>We promise to do our very best to improve your experience of coming, settling and studying in Manchester. \
-		There cannot be a guarantee of how events will occur but we guarantee that we will do all we can to assist and guide you.</p>"
+		There cannot be a guarantee of how events will occur but we guarantee that we will do all we can to assist and guide you.</p> \
+		<p>Our assistance will be available until October 1st 2017.</p>"
 	},
 	banking: {
-		h3: "Banking Setup Terms & Conditions",
-		body: "<p>This is for University of Manchester students only. VarPass will do everything it can to help \
+		h3: "Terms & Conditions",
+		body: "<p>This is for Manchester students only. VarPass will do everything it can to help \
 		you in any way we can. We can't take responsibility for your University application, your accommodation \
 		application, your visa application, or any other administration prior to coming to study in the UK.</p>\
 		<p>We promise to do our very best to improve your experience of coming, settling and studying in Manchester. \
-		There cannot be a guarantee of how events will occur but we guarantee that we will do all we can to assist and guide you.</p>"
+		There cannot be a guarantee of how events will occur but we guarantee that we will do all we can to assist and guide you.</p> \
+		<p>Our assistance will be available until October 1st 2017.</p>"
 	},
 	insurance: {
-		h3: "Problems Insurance Terms & Conditions",
-		body: "<p>This is for University of Manchester students only. VarPass will do everything it can to help \
+		h3: "Terms & Conditions",
+		body: "<p>This is for Manchester students only. VarPass will do everything it can to help \
 		you in any way we can. We can't take responsibility for your University application, your accommodation \
 		application, your visa application, or any other administration prior to coming to study in the UK.</p>\
 		<p>We promise to do our very best to improve your experience of coming, settling and studying in Manchester. \
-		There cannot be a guarantee of how events will occur but we guarantee that we will do all we can to assist and guide you.</p>"
+		There cannot be a guarantee of how events will occur but we guarantee that we will do all we can to assist and guide you.</p> \
+		<p>Our assistance will be available until October 1st 2017.</p>"
 	},
 	affiliate:{
-		h3: "Affiliate Terms & Conditions",
+		h3: "Terms & Conditions",
 		body: "<p>We will pay you 1/3 of all fees referred through your affiliate link. This will be tracked using a cookie \
 		on the user's browser. We will only track the first affiliate link used by a user, therefore if yours is used subsequently \
 		it will not count as your referral. Payment will be issued at the end of September, on valid referals. If you believe there \
